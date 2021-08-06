8250ac77e977c8d2ed12fbf49bd7b4de753d3d6d
Pierre Breny <pierre.breny@gmail.com>
2021-08-06 16:00:27 +0200

    Bon commit

--- backend/controller/productsControllers.js
+++ backend/controller/productsControllers.js
@@ -4,7 +4,8 @@ const getAllProducts = async (req, res) => {
     try {
         const products = await Product.find({});
 
-        res.json(products);
+        // res.json(products);
+        res.send(products);
     } catch (error) {
         console.error(error);
         res.status(500).json({message: "Server Error"});
