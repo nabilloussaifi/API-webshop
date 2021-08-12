c89b5966282a7dc8e64af27df83dfbc511375271
Loussaifi <nabil.loussaifi@gmail.com>
2021-08-12 11:50:41 +0200

    connect front req with back database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-
+mongoose.set('useFindAndModify', false);
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
